2b188a054b41d347c91a9f2f1e2b765085f1acea
Ivan Yordanov <jordanov.ivan@gmail.com>
2019-01-26 17:47:05 +0400

    Add Bridging header

---
+++ BitTicker/BitTicker/Resources/BitTicker-Bridging-Header.h
@@ -0,0 +1,10 @@
+#ifndef iMarvel_Bridging_Header_h
+#define iMarvel_Bridging_Header_h
+
+/// Reachability
+#import "Reachability.h"
+
+/// SVProgressHUD
+#import <SVProgressHUD/SVProgressHUD.h>
+
+#endif
