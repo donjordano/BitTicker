fa239fa8ae945df9d112601963dcce36f768fd14
Ivan Yordanov <jordanov.ivan@gmail.com>
2019-01-26 17:48:23 +0400

    Pair data and Model

---
+++ BitTicker/BitTicker/Models/Ticker.swift
@@ -0,0 +1,20 @@
+//
+//  Ticker.swift
+//  BitTicker
+//
+//  Created by Ivan Yordanov on 1/26/19.
+//  Copyright © 2019 Ivan Yordanov. All rights reserved.
+//
+
+import Foundation
+
+struct Ticker {
+    var tickerId: String
+    var lastPrice: Double
+    var lowestAsk: Double
+    var higestAsk: Double
+    var percent24: Double
+    var higestTrade24: Double
+    var lowestTrade24: Double
+    var isFrozen: Bool
+}
