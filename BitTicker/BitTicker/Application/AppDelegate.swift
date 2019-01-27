a117c186a56f7f14f3c84aa656ad00db569cb31d
Ivan Yordanov <jordanov.ivan@gmail.com>
2019-01-27 22:46:47 +0400

    Refactor AppDelegate

--- BitTicker/BitTicker/Application/AppDelegate.swift
+++ BitTicker/BitTicker/Application/AppDelegate.swift
@@ -17,9 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        RootRouter().presentLoginScreen(in: window!)
-
+        showInitialView()
+        configureNavigationBar()
+        
         /// Firebase config
         FirebaseApp.configure()
     
@@ -40,7 +40,69 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
     }
+}
 
+//MARK: - Navigation methods
+extension AppDelegate  {
+    
+    private func showInitialView() {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        RootRouter().presentLoginScreen(in: window!)
+    }
+    
+    private func configureNavigationBar() {
+        UINavigationBar.appearance().barStyle = .default
+        UINavigationBar.appearance().barTintColor = .darkBlue()
+        UINavigationBar.appearance().tintColor = .white()
+        UINavigationBar.appearance().barStyle = .blackOpaque
+    }
+    
+}
 
+//MARK: - Reachability
+extension AppDelegate {
+//
+//    private func configureReachability() {
+//        NotificationCenter.default.addObserver(self, selector: #selector(handleNetworkChange), name: NSNotification.Name.reachabilityChanged, object: nil)
+//        reachability = Reachability.forInternetConnection()
+//        reachability?.startNotifier()
+//    }
+//
+//    @objc private func handleNetworkChange() {
+//        if !ReachabilityManager.shared.hasConnection() {
+//            showReachabilityMessage(true)
+//            return
+//        }
+//
+//        showReachabilityMessage(false)
+//    }
+//
+//    /**
+//     * Show reachability message
+//     *
+//     * - parameters:
+//     *      -show: show / hide reachability message
+//     */
+//    private func showReachabilityMessage(_ show: Bool) {
+//        guard let rootViewController = UIApplication.shared.windows[0].rootViewController else {
+//            return
+//        }
+//
+//        if show, let _ = rootViewController.presentedViewController as? GeneralMessageViewController {
+//            return
+//        }
+//
+//        if show {
+//            let generalMessageViewController = GeneralMessageViewController()
+//            generalMessageViewController.modalTransitionStyle = .coverVertical
+//            generalMessageViewController.modalPresentationStyle = .overCurrentContext
+//            generalMessageViewController.presenter = GeneralMessagePresenter(view: generalMessageViewController, type: .NoInternetConnection)
+//            rootViewController.present(generalMessageViewController, animated: true, completion: nil)
+//        } else {
+//            rootViewController.dismiss(animated: true, completion: nil)
+//        }
+//    }
+    
 }
 
