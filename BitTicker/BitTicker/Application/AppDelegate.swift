dc3bed2c94ded75143754057c79084ec0d4744f9
Ivan Yordanov <jordanov.ivan@gmail.com>
2019-01-27 10:16:42 +0400

    Adding new files to the project

--- BitTicker/BitTicker/Application/AppDelegate.swift
+++ BitTicker/BitTicker/Application/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         RootRouter().presentLoginScreen(in: window!)
-        
+
         /// Firebase config
         FirebaseApp.configure()
     
